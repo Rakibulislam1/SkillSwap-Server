c0763baaa98b9895590aad8f3af7b7561f7c7c38
Rakibulislam1 <rirakib03@gmail.com>
2023-11-08 18:39:52 +0600

    service delete by user

--- Index.js
+++ Index.js
@@ -131,6 +131,10 @@ async function run() {
       res.send(result);
     });
 
+    // delete
+    app.delete("/api/v1/services/:id", async (req, res) => 
+    {const id = req.params.id;const query = {_id: new ObjectId(id) };
+    const result = await serviceCollection.deleteOne(query);res.send(result);});
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
