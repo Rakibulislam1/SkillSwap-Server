9047d3af212730b8f33158c5a01b8fb7a163645e
Rakibulislam1 <rirakib03@gmail.com>
2023-11-08 23:04:29 +0600

    jwt added

--- Index.js
+++ Index.js
@@ -2,17 +2,20 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
 require("dotenv").config();
 
-const secret = "mostimportantveryimportant";
+// const secret = "mostimportantveryimportant";
 
 // json parser
 app.use(express.json());
-app.use(cors({
-  origin:["http://localhost:5173"], credentials:true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 // cookie parser
 app.use(cookieParser());
@@ -21,14 +24,14 @@ app.use(cookieParser());
 const gateman = (req, res) => {
   const token = req.cookies;
 
-  if(!token){
-    return res.status(401).send({message: 'You are not authorized'})
+  if (!token) {
+    return res.status(401).send({ message: "You are not authorized" });
   }
 
   // verify a token symmetric
   jwt.verify(token, secret, function (err, decoded) {
-    if(err){
-      return res.status(401).send({message: 'You are not authorized'}) 
+    if (err) {
+      return res.status(401).send({ message: "You are not authorized" });
     }
   });
 };
@@ -53,7 +56,6 @@ async function run() {
     const serviceCollection = client.db("swap").collection("services");
     const serviceBooking = client.db("swap").collection("bookings");
 
-
     // all services data
     app.get("/api/v1/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -61,60 +63,58 @@ async function run() {
       res.send(result);
     });
 
-   // post services in data base
-   app.post("/api/v1/services", async (req, res) => {
-    const services = req.body;
-    console.log(services);
-    const result = await serviceCollection.insertOne(services);
-    res.send(result);
-  })
+    // post services in data base
+    app.post("/api/v1/services", async (req, res) => {
+      const services = req.body;
+      console.log(services);
+      const result = await serviceCollection.insertOne(services);
+      res.send(result);
+    });
 
-  //service bookings
-  app.post("/api/v1/bookings", async (req, res) => {
-    const booking = req.body;
-    console.log(booking);
-    const result = await serviceBooking.insertOne(booking);
-    res.send(result);
-  })
-  
+    //service bookings
+    app.post("/api/v1/bookings", async  (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await serviceBooking.insertOne(booking);
+      res.send(result);
+    });
+
     // service booking user email based
     app.get("/api/bookings/:userEmail", async (req, res) => {
       console.log(req.query.email);
-    
-      const userEmail = req.params.userEmail; 
 
-      const result = await serviceBooking.find({userEmail}).toArray();
+      const userEmail = req.params.userEmail;
+
+      const result = await serviceBooking.find({ userEmail }).toArray();
       res.send(result);
-
     });
 
     // service booking get
     app.get("/api/services/:userEmail", async (req, res) => {
       console.log(req.query.email);
-    
-      const userEmail = req.params.userEmail; 
 
-      const result = await serviceBooking.find({userEmail}).toArray();
+      const userEmail = req.params.userEmail;
+
+      const result = await serviceBooking.find({ userEmail }).toArray();
       res.send(result);
-
     });
 
-    // specific service 
+    // specific service
     app.get("/api/v1/services/:service_provider_email", async (req, res) => {
-    
       const service_provider_email = req.params.service_provider_email;
 
-      const result = await serviceCollection.find({service_provider_email}).toArray();
+      const result = await serviceCollection
+        .find({ service_provider_email })
+        .toArray();
       res.send(result);
-
     });
 
     // update
     app.put("/api/v1/services/:id", async (req, res) => {
       const id = req.params.id;
       const data = req.body;
-      const filter = {_id: new ObjectId(id)};
-      const options = { upsert: true }; 
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateService = {
         $set: {
           service_name: data.service_name,
@@ -132,22 +132,27 @@ async function run() {
     });
 
     // delete
-    app.delete("/api/v1/services/:id", async (req, res) => 
-    {const id = req.params.id;const query = {_id: new ObjectId(id) };
-    const result = await serviceCollection.deleteOne(query);res.send(result);});
+    app.delete("/api/v1/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
 
-       // jwt
-       app.post("/jwt", (req, res) => {
-        // create token and send to client
-        const user = req.body;
-        const token = jwt.sign(user, process.env.SECRET_KEY,{expiresIn: "24h"});
-        res
-          .cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-          })
-          .send({ success: true });
+    // jwt
+    app.post("/jwt", (req, res) => {
+      // create token and send to client
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_KEY, {
+        expiresIn: "24h",
       });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
